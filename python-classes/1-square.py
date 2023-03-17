0f2c901e8d751b5a9e415120f6e42758d8998a90
Dimitri250 <k.dimitri@alustudent.com>
2023-03-17 08:47:08 -0700

    size of the square

---
+++ python-classes/1-square.py
@@ -0,0 +1,8 @@
+"""Write a class Square that defines a square by"""
+
+
+class Square:
+    """Private instance attribute: size
+    Instantiation with size (no type/value verification)"""
+    def __init__(self, size):
+        self.__size = size
