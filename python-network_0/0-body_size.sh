f7ea8aad98f3f6a4f03b1be07f8a5d89cbd4e829
Dimitri250 <118120317+Dimitri250@users.noreply.github.com>
2023-04-07 16:07:29 +0200

    Update 0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,2 +1,3 @@
-sh script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+#!/bin/bash
+# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
 curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
