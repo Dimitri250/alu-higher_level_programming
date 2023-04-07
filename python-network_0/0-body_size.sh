094fdcb211f4ef8f90a59497c60c836e0b52bd7a
Dimitri250 <118120317+Dimitri250@users.noreply.github.com>
2023-04-07 17:12:33 +0200

    Update 0-body_size.sh

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sS "$1" | wc -c | xargs echo "The size of the response body is: {} bytes"
+# Getting started with curl
+curl -sI "$1" | grep 'Content-Length' | cut -d " " -f2;
