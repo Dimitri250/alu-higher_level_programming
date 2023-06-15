f27e7a0a7a2f27ebd442552180e827e60a901265
Dimitri250 <118120317+Dimitri250@users.noreply.github.com>
2023-06-15 14:04:16 +0300

    Update 2-arguments.js

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,7 +4,7 @@ const message = process.argv.slice(2);
 
 if (message.length === 0) {
   console.log('No argument');
-} else if (messsage.length === 1) {
+} else if (message.length === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
