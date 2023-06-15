5edde2a08d17ff9f5595ed60bd258be06197d6f0
Dimitri250 <k.dimitri@alustudent.com>
2023-06-15 10:38:20 +0000

    1

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const string1 = 'C is fun';
+const string2 = 'Python is cool';
+const string3 = 'JavaScript is amazing';
+console.log(string1);
+console.log(string2);
+console.log(string3);
